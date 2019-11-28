2c358bc774cad2b1f3b8bd4a976039845c7804b4
Aaron <aarljl1994@gmail.com>
2019-11-28 20:56:20 +0800

    Enemy Behaviour

---
+++ source/ktwgEngine/EnemyBehaviour.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "behaviour.h"
+
+class EnemyBehaviour : public Behaviour
+{
+public:
+  EnemyBehaviour(Entity& entity);
+  ~EnemyBehaviour();
+
+  void Start() override;
+  void Update() override;
+
+private:
+
+  void ChaseTarget(const Vec3& currentPos);
+  void UpdateTarget(const Vec3& currentPos);
+
+  Entity* m_Target;
+  float m_CurrentTime;
+  float m_PollingInterval;
+  float m_Speed;
+};
