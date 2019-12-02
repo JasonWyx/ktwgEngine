82f43819910c043d1d32fc5028465b0d29a05df2
wenlinkoh <wenlin.koh@digipen.edu>
2019-12-02 21:09:41 +0800

    Added simple camera controls

--- source/ktwgEngine/camerabehaviourscript.cpp
+++ source/ktwgEngine/camerabehaviourscript.cpp
@@ -21,7 +21,38 @@ void CameraBehaviour::Start()
 void CameraBehaviour::Update()
 {
   Vec3 pos = GetTransform().GetPosition();
-  pos.x_ = m_Target->GetTransform().GetPosition().x_;
-  pos.z_ = m_Target->GetTransform().GetPosition().z_;
+  
+  if (m_Target)
+  {
+	  pos.x_ = m_Target->GetTransform().GetPosition().x_;
+	  pos.z_ = m_Target->GetTransform().GetPosition().z_;
+	  GetTransform().SetPosition(pos);
+  }
+
+  if (Input().OnKeyPress(KTWG_W))
+  {
+    pos.z_ -= 1.0f;
+  }
+  if (Input().OnKeyPress(KTWG_A))
+  {
+    pos.x_ -= 1.0f;
+  }
+  if (Input().OnKeyPress(KTWG_S))
+  {
+    pos.z_ += 1.0f;
+  }
+  if (Input().OnKeyPress(KTWG_D))
+  {
+    pos.x_ += 1.0f;
+  }
+  if (Input().OnKeyPress(KTWG_C))
+  {
+    pos.y_ -= 1.0f;
+  }
+  if (Input().OnKeyPress(KTWG_V))
+  {
+    pos.y_ += 1.0f;
+  }
+
   GetTransform().SetPosition(pos);
 }
