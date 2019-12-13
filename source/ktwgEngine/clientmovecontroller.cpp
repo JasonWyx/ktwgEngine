e23e9736153008387a2df4803c2240aae9a3820f
wenlinkoh <wenlin.koh@digipen.edu>
2019-12-13 20:15:17 +0800

    fix

--- source/ktwgEngine/clientmovecontroller.cpp
+++ source/ktwgEngine/clientmovecontroller.cpp
@@ -36,7 +36,7 @@ void ClientMoveController::Update()
       PeerID peerID = StreamManager::GetInstance().GetPeerID();
       PlayerReadyEvent* playerReadyEvent = new PlayerReadyEvent;
       playerReadyEvent->m_SourcePeerID = peerID;
-      PlayerReadyEvent->m_Ready = true;
+      playerReadyEvent->m_Ready = true;
       StreamManager::GetInstance().GetEventManager().BroadcastEvent(playerReadyEvent, true);
       m_GSManager->SetReadyActive(!m_GSManager->GetReadyActive());
     }
